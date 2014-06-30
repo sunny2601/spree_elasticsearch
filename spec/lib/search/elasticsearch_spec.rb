2d3035f26aef16bdae69ca408c4c0499969ccee1
kenleytomlin <kenleytomlin@gmail.com>
2014-06-30 15:46:32 +0100

    Added simple search tests

--- spec/lib/search/elasticsearch_spec.rb
+++ spec/lib/search/elasticsearch_spec.rb
@@ -10,6 +10,29 @@ describe Spree::Search::Elasticsearch do
     it "should find the women's shoe" do
       searcher = Spree::Search::Elasticsearch.new(:keywords => "women's", :per_page => "")
       response = searcher.retrieve_products
+      expect(response.to_a.length).to eql(1)
+      expect(response.to_a.first.name).to eql(@product1.name)
+    end
+
+    it "should find the mens shoe" do
+      search = Spree::Search::Elasticsearch.new(:keywords => "men's", :per_page => "")
+      response = search.retrieve_products
+      expect(response.to_a.length).to eql(1)
+      expect(response.to_a.first.name).to eql(@product2.name)
+    end
+
+    it "should find the mens and womens shoes, but the womens shoe should be first" do
+      search = Spree::Search::Elasticsearch.new(:keywords => "women's golf shoe", :per_page => "")
+      response = search.retrieve_products
+      expect(response.to_a.length).to eql(2)
+      expect(response.to_a.first.name).to eql(@product1.name)
+    end
+
+    it "should find the mens and womens shoes, but the mens shoe should be first" do
+      search = Spree::Search::Elasticsearch.new(:keywords => "men's golf shoe", :per_page => "")
+      response = search.retrieve_products
+      expect(response.to_a.length).to eql(2)
+      expect(response.to_a.first.name).to eql(@product2.name)
     end
   end
 end
