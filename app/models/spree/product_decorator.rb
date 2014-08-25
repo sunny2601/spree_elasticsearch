5d9d0ed469f65e9864d10d40dd8393873c265608
JonghunYu <yuzong@naver.com>
2014-08-25 19:23:00 +0900

    add simple scope descend_by_created_at

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,11 +5,12 @@ Spree::Product.class_eval do
   index_name "#{ENV['RAILS_ENV'] || "development"}_#{Spree::Config.site_name.downcase.gsub " ","_"}"
   document_type 'product'
 
+  add_simple_scopes [:descend_by_created_at]
+
   def self.es_search(query)
     response = self.__elasticsearch__.search query
     response
   end
-
   def as_indexed_json(options={})
     if !self.images.empty?
       image_url = self.images.first.attachment.url
