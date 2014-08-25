59e5dcab940c6026fb06246410ceb360f7a4e3a1
JonghunYu <yuzong@naver.com>
2014-08-25 19:22:18 +0900

    add order by created_at in base scope

--- lib/spree/search/elasticsearch.rb
+++ lib/spree/search/elasticsearch.rb
@@ -24,7 +24,7 @@ module Spree
        protected
          def get_base_scope
            if keywords.nil?
-             base_scope = Spree::Product.active
+             base_scope = Spree::Product.active.descend_by_created_at
              base_scope = base_scope.in_taxon(taxon) unless taxon.blank?
              base_scope = add_search_scopes(base_scope)
              base_scope
